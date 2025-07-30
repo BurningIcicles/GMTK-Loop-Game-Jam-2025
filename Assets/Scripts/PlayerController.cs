6d081075add8e0d6564e0bbecd976c65ccb858f9
Richies Huynh <richies2006huynh@gmail.com>
2025-07-30 15:36:42 -0700

    Movement finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
+    private BoxCollider2D _boxCollider;
+    [SerializeField]
+    private int speed = 40; // default value of 40
+    [SerializeField]
+    private float jumpInitialVelocity = 10;
+
+    private float _jumpVelocity;
+    private Vector2 _dirJump;
+    private bool _isGrounded;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _isGrounded = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        transform.position += movement * (Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.Space) && _isGrounded)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(Vector2.up*jumpInitialVelocity, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            _isGrounded = true;
+        }
+    }
+  
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            _isGrounded = false;
+        }
     }
 }
