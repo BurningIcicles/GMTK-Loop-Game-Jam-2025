f1d1ac800e845dc08057b2d2acf92eef5589503a
Jaedon Bersamin <Birdman.B1324@gmail.com>
2025-08-03 04:23:45 -0700

    Revert "Merge branch 'master' of https://github.com/JaedonBersamin/GMTK-Loop-Game-Jam-2025 into jaedon"
    This reverts commit 5f93bb26f276da5203205e0dc3484b6fcaa2a5c1, reversing
    changes made to 13b8f9c6fccaf4151e0f7ddd67f4edcdb6b3662f.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,8 @@
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    private static readonly int Move1 = Animator.StringToHash("Move");
-    private static readonly int Idle1 = Animator.StringToHash("Idle");
     private Rigidbody2D _rigidbody;
     private BoxCollider2D _boxCollider;
     private SceneController _sceneController;
@@ -23,8 +20,6 @@ public class PlayerController : MonoBehaviour
     private bool _isWalking;
     [SerializeField]
     private bool isFloating;
-
-    private Animator _playerAnimator;
     private Animator _loopAnimator;
     private SpriteRenderer _loopSprite;
     
@@ -36,7 +31,6 @@ public class PlayerController : MonoBehaviour
         _boxCollider = gameObject.GetComponent<BoxCollider2D>();
         isGrounded = true;
         _isWalking = false;
-        _playerAnimator = gameObject.GetComponent<Animator>();
         _loopAnimator = transform.Find("Loop").GetComponent<Animator>();
         _loopSprite = transform.Find("Loop").GetComponent<SpriteRenderer>();
     }
@@ -59,13 +53,7 @@ public class PlayerController : MonoBehaviour
         }
 
         if (Input.GetAxisRaw("Horizontal") != 0)
-        {
             Move();
-        }
-        else
-        {
-            Idle();
-        }
 
         if (Input.GetKey(KeyCode.Space) && isGrounded)
             Jump();
@@ -82,24 +70,6 @@ public class PlayerController : MonoBehaviour
         {
             Ground();
         }
-        _playerAnimator.SetTrigger(Move1);
-        
-        Vector3 scale = transform.localScale;
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            scale.x = -1;
-        }
-        else
-        {
-            scale.x = 1;
-        }
-
-        transform.localScale = scale;
-    }
-
-    private void Idle()
-    {
-        _playerAnimator.SetTrigger(Idle1);
     }
 
     private void Spring()
